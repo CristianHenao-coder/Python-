69374068d606aad5ba6d1076e14b9e72986c971f
CristianHenao-coder <cristiancloser7@gmail.com>
2025-08-09 20:02:57 -0500

    Ejercicios Python

---
+++ Python-Exercise/Basicos/1.1  Intercambio de variables.py
@@ -0,0 +1,9 @@
+# Ejercicios Python 1.1 
+
+# 1 Intercambio de variables
+
+# Numero1= int(input("Ingresa un numero sin comas ni puntos :"))
+# Numero2= int(input("Ingresa un segundo numero sin comas ni puntos :"))
+# print("numeros ingresados" )
+# print("A = " , Numero2  ) 
+# print("B = " , Numero1)
